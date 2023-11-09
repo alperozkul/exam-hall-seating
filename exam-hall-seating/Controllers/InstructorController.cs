d22b06332f2fa1ae6e39c22288efce76f3a5540a
Alper Özkul <alper.ozkul@bil.omu.edu.tr>
2023-11-09 13:35:56 +0300

    Add controller and view files

---
+++ exam-hall-seating/Controllers/InstructorController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace exam_hall_seating.Controllers
+{
+    public class InstructorController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ exam-hall-seating/Controllers/LectureController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace exam_hall_seating.Controllers
+{
+    public class LectureController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
