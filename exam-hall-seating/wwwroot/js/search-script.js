d25d7ef1b6fece230b21b7f4135fe5be5d4e34f2
Alper Özkul <alper.ozkul@bil.omu.edu.tr>
2023-11-27 15:38:49 +0300

    Search added.

---
+++ exam-hall-seating/wwwroot/js/search-script.js
@@ -0,0 +1,9 @@
+﻿$(document).ready(function () {
+    $("#searchInput").on("input", function () {
+        var searchText = $(this).val().toLowerCase();
+
+        $("tbody tr").filter(function () {
+            $(this).toggle($(this).text().toLowerCase().indexOf(searchText) > -1);
+        });
+    });
+});
